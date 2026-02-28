01c6ba59558f1fe8675ccd352a4ff55bce8b3f61
SHUVRADIP SAHA ROY <shuvradipsaharoy2006@gmail.com>
2026-02-28 12:53:39 +0530

    Add files via upload

---
+++ FunGreeting.java
@@ -0,0 +1,6 @@
+public class FunGreeting {
+    public static void main(String[] args) {
+        String teacherName = "Anjan Kumar Payra";
+        System.out.println("Hello " + teacherName + "! Mr sayan debnath , shall I give you a blanket for sleeping?");
+    }
+}
